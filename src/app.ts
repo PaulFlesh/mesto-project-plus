0503e7c00f3b8cce5e54ca173970741e1b4129a4
Johnny Blade <nikitapiskurev@gmail.com>
2023-07-19 17:57:14 +0400

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Response } from 'express';
 import mongoose from 'mongoose';
 import cardRouter from './routes/cards';
 import userRouter from './routes/users';
+import { NOT_FOUND } from 'constants/codes';
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -18,5 +19,9 @@ app.use((req: any, res: Response, next) => {
 
 app.use('/cards', cardRouter);
 app.use('/users', userRouter);
+
+app.use((req: Request, res: Response) => {
+  res.status(NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT);
