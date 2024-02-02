c2fba5369945541798ba2abd332d8a335996c55d
Johnny Blade <notjohnnyblade@yandex.ru>
2024-02-02 13:46:38 +0400

    Changed dbconn string from localhost to 127.0.0.1

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
 
