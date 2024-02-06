16f6c9df80acc68fd9f9935b298af22c9f2461c6
Johnny Blade <notjohnnyblade@yandex.ru>
2024-02-06 15:40:31 +0400

    test scp

--- scripts/deployEnv.sh
+++ scripts/deployEnv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SSH_CONFIG="${1}"
 PROJECT_PATH="${2}"
-scp -i "C:\Users\M2entertainment/.ssh/id_ed25519_yacloud" -Cr .env "$SSH_CONFIG:${PROJECT_PATH}/current"
+scp -Cr .env "$SSH_CONFIG:${PROJECT_PATH}/current"
